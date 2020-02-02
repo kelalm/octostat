cb2fdfe0f8776525fae89ebcebe734a0e2d09c47
kelalm <kelvinalmonte@live.com>
2020-02-02 16:31:12 -0500

    implemented react hooks!

--- components/MainRow.js
+++ components/MainRow.js
@@ -1,27 +1,25 @@
 import { Card, Col, Row } from "antd";
 
-const MainRow = props => {
-  return (
-    <div style={{ background: "#ECECEC", padding: "30px" }}>
-      <Row gutter={16}>
-        <Col span={8}>
-          <Card title="Card title" bordered={false}>
-            Card content
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card title="Card title" bordered={false}>
-            Card content
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card title="Card title" bordered={false}>
-            Card content
-          </Card>
-        </Col>
-      </Row>
-    </div>
-  );
-};
+const MainRow = ({ userData }) => (
+  <div style={{ background: "#ECECEC", padding: "30px" }}>
+    <Row gutter={16}>
+      <Col span={8}>
+        <Card title="Total Repositories" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Followers" bordered={false}>
+          {userData.name}
+        </Card>
+      </Col>
+      <Col span={8}>
+        <Card title="Following" bordered={false}>
+          Card content
+        </Card>
+      </Col>
+    </Row>
+  </div>
+);
 
 export default MainRow;
--- pages/user/[username].js
+++ pages/user/[username].js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 import { Layout, Menu, Breadcrumb } from "antd";
 import { List, Avatar, Icon } from "antd";
 import { Row, Col } from "antd";
@@ -15,6 +17,79 @@ var GhPolyglot = require("gh-polyglot");
 const { Title } = Typography;
 
 const Stats = props => {
+  const username = "kelalm";
+  const [userData, setUserData] = useState("");
+  const [langData, setLangData] = useState("");
+  const [repoData, setRepoData] = useState("");
+  const [error, setError] = useState({ active: false, type: 200 });
+  const [rateLimit, setRateLimit] = useState(null);
+
+  const getUserData = () => {
+    fetch(`https://api.github.com/users/${username}`)
+      .then(response => {
+        if (response.status === 404) {
+          return setError({ active: true, type: 404 });
+        }
+        if (response.status === 403) {
+          return setError({ active: true, type: 403 });
+        }
+        return response.json();
+      })
+      .then(json => setUserData(json))
+      .catch(error => {
+        setError({ active: true, type: 400 });
+        console.error("Error:", error);
+      });
+  };
+
+  const getLangData = () => {
+    const me = new GhPolyglot(`${username}`);
+    me.userStats((err, stats) => {
+      if (err) {
+        console.error("Error:", err);
+        setError({ active: true, type: 400 });
+      }
+      setLangData(stats);
+    });
+  };
+
+  const getRepoData = () => {
+    fetch(`https://api.github.com/users/${username}/repos?per_page=100`)
+      .then(response => {
+        if (response.status === 404) {
+          return setError({ active: true, type: 404 });
+        }
+        if (response.status === 403) {
+          return setError({ active: true, type: 403 });
+        }
+        return response.json();
+      })
+      .then(json => setRepoData(json))
+      .catch(error => {
+        setError({ active: true, type: 200 });
+        console.error("Error:", error);
+      });
+  };
+
+  useEffect(() => {
+    // fetch(`https://api.github.com/rate_limit`)
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     setRateLimit(json.resources.core);
+    //     if (json.resources.core.remaining < 1) {
+    //       setError({ active: true, type: 403 });
+    //     }
+    //   });
+
+    // getUserData();
+    // getLangData();
+    // getRepoData();
+
+    setUserData({ name: "kelvin" });
+    // setLangData(mockLangData);
+    // setRepoData(mockRepoData);
+  }, []);
+
   return (
     <Layout>
       <Content
@@ -28,44 +103,17 @@ const Stats = props => {
         }}
       >
         <div>
-          <Title>{props.username}</Title>
-          <Title>{props.username}</Title>
+          <Title>{username}</Title>
+          <Title>{username}</Title>
           <h6>Software Engineer from San Francisco</h6>
           <p>Joined on September 12th, 2013</p>
           <p>New York</p>
-
-          <MainRow />
+          <MainRow userData={userData} />
           <TopRepos />
         </div>
       </Content>
     </Layout>
   );
-};
-
-Stats.getInitialProps = async function(context) {
-  // On a post page, retrive post by id and the comments for that post.
-  const { username } = context.query;
-  // Stats about git-stats
-  var gitUser = new GhPolyglot(username);
-
-  // // Repository stats
-  // gitUser.repoStats(function(err, stats) {
-  //   console.log(err || stats);
-  // });
-
-  // // User stats
-  // gitUser.userStats(function(err, stats) {
-  //   console.log(err || stats);
-  // });
-
-  var allRepos;
-
-  gitUser.getAllRepos(function(err, stats) {
-    allRepos = stats;
-    console.log(stats);
-  });
-
-  return { username, allRepos };
 };
 
 export default Stats;
