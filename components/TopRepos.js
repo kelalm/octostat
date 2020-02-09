06dd0f339d0fb7496d0d1a30ea244d16749f0de1
kelalm <kelvinalmonte@live.com>
2020-02-08 20:34:20 -0500

    added sort types to toprepos component

--- components/TopRepos.js
+++ components/TopRepos.js
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
 import { Card } from "antd";
 
 const gridStyle = {
@@ -9,6 +12,41 @@ const TopRepos = ({ repoData }) => {
   const [topRepos, setTopRepos] = useState([]);
   const [sortType, setSortType] = useState("stars");
   const [dropdownOpen, setDropDownOpen] = useState(false);
+
+  const getTopRepos = type => {
+    const LIMIT = 8;
+
+    const map = {
+      stars: "stargazers_count",
+      forks: "forks_count",
+      size: "size"
+    };
+
+    const sortProperty = map[type];
+    const sorted = repoData
+      .filter(repo => !repo.fork)
+      .sort((a, b) => b[sortProperty] - a[sortProperty])
+      .slice(0, LIMIT);
+
+    setTopRepos(sorted);
+  };
+
+  useEffect(() => {
+    if (repoData.length) {
+      getTopRepos();
+    }
+  }, []);
+
+  useEffect(() => getTopRepos(sortType), [sortType]);
+
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
+  const changeRepoSort = sortType => {
+    setSortType(sortType);
+    toggleDropdown();
+  };
+
+  const sortTypes = ["stars", "forks", "size"];
 
   return (
     <Card title="Top Repositories" style={{ marginTop: 100 }}>
