6973aa0130c3bac3ca301354725dc3b907c98443
kelalm <kelvinalmonte@live.com>
2020-02-11 00:00:59 -0500

    Align card text to center

--- components/MainRow.js
+++ components/MainRow.js
@@ -7,17 +7,32 @@ const MainRow = ({ userData }) => (
   <div style={{ background: "#ECECEC", padding: "30px" }}>
     <Row gutter={16}>
       <Col span={8}>
-        <Card title="Total Repositories" hoverable={true} bordered={false}>
+        <Card
+          title="Total Repositories"
+          bodyStyle={{ textAlign: "center" }}
+          hoverable={true}
+          bordered={false}
+        >
           <Title>{userData.public_repos}</Title>
         </Card>
       </Col>
       <Col span={8}>
-        <Card title="Followers" hoverable={true} bordered={false}>
+        <Card
+          title="Followers"
+          bodyStyle={{ textAlign: "center" }}
+          hoverable={true}
+          bordered={false}
+        >
           <Title>{userData.followers}</Title>
         </Card>
       </Col>
       <Col span={8}>
-        <Card title="Following" hoverable={true} bordered={false}>
+        <Card
+          title="Following"
+          bodyStyle={{ textAlign: "center" }}
+          hoverable={true}
+          bordered={false}
+        >
           <Title>{userData.following}</Title>
         </Card>
       </Col>
