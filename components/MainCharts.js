8defcecfd5cdcc1dabfaf00e3129fc9c5e40d7fe
kelalm <kelvinalmonte@live.com>
2020-02-16 19:58:28 -0500

    Clean up Doughnut data code

--- components/MainCharts.js
+++ components/MainCharts.js
@@ -4,48 +4,6 @@ import { Bar } from "react-chartjs-2";
 
 import DoughnutChart from "./charts/DoughnutChart";
 
-const data2 = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      label: "My First dataset",
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: "rgba(75,192,192,0.4)",
-      borderColor: "rgba(75,192,192,1)",
-      borderCapStyle: "butt",
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: "miter",
-      pointBorderColor: "rgba(75,192,192,1)",
-      pointBackgroundColor: "#fff",
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(75,192,192,1)",
-      pointHoverBorderColor: "rgba(220,220,220,1)",
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
-
-const data3 = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "rgba(255,99,132,0.2)",
-      borderColor: "rgba(255,99,132,1)",
-      borderWidth: 1,
-      hoverBackgroundColor: "rgba(255,99,132,0.4)",
-      hoverBorderColor: "rgba(255,99,132,1)",
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
-
 const MainCharts = ({ langData }) => (
   <div style={{ background: "#FFFFFF", padding: "30px" }}>
     <Row gutter={8}>
--- components/charts/DoughnutChart.js
+++ components/charts/DoughnutChart.js
@@ -1,16 +1,5 @@
 import { Doughnut } from "react-chartjs-2";
 
-const data = {
-  labels: ["Red", "Green", "Yellow"],
-  datasets: [
-    {
-      data: [300, 50, 100],
-      backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-      hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"]
-    }
-  ]
-};
-
 const DoughnutChart = ({ langData }) => {
   const data = {
     labels: langData.map(a => a.label),
